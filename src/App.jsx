e4ada34b419f45d59b40cb293cdd382c41755128
Garv767 <garvrahut@gmail.com>
2026-02-04 23:57:29 +0530

    added a few components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './Card.jsx'
-
+import Card from './Coponents/Card/Card.jsx' 
+import Home from './Coponents/Home/Home.jsx'
+import Header from './Coponents/Header/Header.jsx'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
 function App() {
   return (
-    <>
+    <Router>
+      <Header/>
+      <Home/>
       <Card/>
       <Card/>
-    </>
+    </Router>
   );
 }
 
---
+++ src/Coponents/Card/Card.jsx
@@ -0,0 +1,12 @@
+import './card.css';
+
+function Card() {
+  return (
+    <div className="card">
+      <h2>Card Component</h2>
+      <p>This is a simple card component.</p>
+    </div>
+  );
+}
+
+export default Card;
