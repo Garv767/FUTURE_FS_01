96c10752a3a9b9f4fa4730342d0dae6b97c9fcef
Garv767 <garvrahut@gmail.com>
2026-08-10 12:00:00 +0530

    fix: add lazy loading to project images

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -9,7 +9,7 @@ import { BiGitRepoForked } from "react-icons/bi";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" loading="lazy" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
